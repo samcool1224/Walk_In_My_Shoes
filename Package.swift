9686ebc6698adfd7926e932948c7445b15b6c32a
samcool1224 <samaksh2405@gmail.com>
2025-02-24 01:57:54 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,30 +1,29 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5  // Adding this line to specify Swift version
 import PackageDescription
 
 let package = Package(
     name: "WalkInMyShoes",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WalkInMyShoes",
             targets: ["WalkInMyShoes"]),
     ],
+    dependencies: [  // Moving dependencies to package level
+        .package(url: "https://github.com/samcool1224/Walk_In_My_Shoes.git", from: "1.0.0")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "WalkInMyShoes",
+            dependencies: ["WalkInMyShoes"],  // Adding dependencies to target
             path: ".",
             resources: [
-                .process("Assets.xcaassets")
+                .process("Assets.xcassets"),  // Added missing comma
                 .process("Fonts")
-                ],)
-        dependencies: [.package(url: "https://github.com/samcool1224/Walk_In_My_Shoes.git", from: "1.0.0")]
+            ]
+        ),
         .testTarget(
             name: "WalkInMyShoesTests",
             dependencies: ["WalkInMyShoes"]
-        ),
+        )
     ]
 )
