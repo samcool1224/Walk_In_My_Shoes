6250f4e6147d7e3ec875362a91dfa862e48142ea
samcool1224 <samaksh2405@gmail.com>
2025-02-24 23:35:53 -0500

    changed

--- WalkInMyShoes.swift
+++ WalkInMyShoes.swift
@@ -2,19 +2,18 @@
 // https://docs.swift.org/swift-book
 import SwiftUI
 @available(iOS 17, *)
-
-public struct WalkInMyShoesApp: App {
-    public init() {}
+public struct WalkInMyShoesView: View {
     @AppStorage("uiTheme") private var uiTheme: Int = 1
-
-    public var body: some SwiftUI.Scene {
+    
+    public init() {}
+    
+    public var body: some View {
         let theme: ColorScheme? = uiTheme == 2 ? .dark : (uiTheme == 1 ? .light : nil)
         
-        return WindowGroup {
-            NavigationView {
-                ContentView()
-                    .preferredColorScheme(theme)
-            }
+        NavigationView {
+            ContentView()  // You can call any view here
+                .preferredColorScheme(theme)
         }
     }
 }
+
